caa4bfdaa77bebf3cdca89b50178e8466ef698c8
huangmengyao <2594838054@qq.com>
2023-09-10 17:42:43 +0800

    feat: 渲染导航侧边栏

--- src/views/layout/NavBar.vue
+++ src/views/layout/NavBar.vue
@@ -4,13 +4,14 @@
       @open="handleOpen"
       @close="handleClose"
       :unique-opened="true" 控制是否只保持一个子菜单的展开
+      default-active="2" 默认初识时菜单2展开
      -->
      <h1 class="main-logo">
       <img src="../../assets/images/logo.png" alt="" width="32px">
       <span v-show="!isCollapse">通用后台管理系统</span>
      </h1>
     <el-menu
-      default-active="2"
+      :router="true"
       class="el-menu-vertical-demo"
       background-color="#054722"
       :collapse="isCollapse"
@@ -18,28 +19,20 @@
       :collapse-transition="false"
       text-color="#fff"
       active-text-color="#ffd04b">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航三</span>
-        </template>
-        <el-menu-item index="3-1">选项1</el-menu-item>
-        <el-menu-item index="3-2">选项2</el-menu-item>
-        <el-menu-item index="3-3">选项3</el-menu-item>
-      </el-submenu>
+      <div v-for="item,idx in menuData" :key="idx">
+        <el-submenu :index="item.path" v-if="item.children">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>{{ item.title }}</span>
+          </template>
+          <el-menu-item :index="sitem.path" v-for="sitem,sidx in item.children" :key="sidx">{{ sitem.title }}</el-menu-item>
+        </el-submenu>
+
+        <el-menu-item :index="item.path" v-else>
+          <i class="el-icon-menu"></i>
+          <span slot="title">{{ item.title }}</span>
+        </el-menu-item>
+      </div>
     </el-menu>
   </div>
 </template>
@@ -49,7 +42,36 @@ import { mapState } from 'vuex'
 export default {
   data(){
     return {
-      
+      menuData:[
+        {
+          title:"首页",
+          path:"/",
+        },
+        {
+          title:"客户管理",
+          path:"/customer",
+          children:[
+            {title:"客户档案",path:"/customer/customer",},
+            {title:"拜访记录",path:"/customer/visit",},
+          ],
+        },
+        {
+          title:"修养预约",
+          path:"/business",
+          children:[
+            {title:"预约信息",path:"/business/appointment",},
+            {title:"服务项",path:"/business/service",},
+            {title:"结算单",path:"/business/statement",},
+          ],
+        },
+        {
+          title:"流程管理",
+          path:"/flow",
+          children:[
+            {title:"审核流程定义",path:"/flow/definition",},
+          ]
+        }
+      ],
     }
   },
   computed:{
@@ -87,9 +109,14 @@ export default {
       font-weight: bold;
       display: inline-block;
       position: absolute;
-      left: 62px;
+      left: 60px;
       top: 16px;
     }
   }
+}
+
+// 解决添加div后的样式显示bug
+.isColl .el-submenu__title span, ::v-deep .el-menu--collapse .el-submenu__title .el-submenu__icon-arrow{
+  display: none;
 }
 </style>
